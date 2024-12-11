28412ef8fb6e13fb1320d5fbbe86b6cedc7f77d2
纪华裕 <jihuayu123@gmail.com>
2024-12-11 01:12:15 +0000

    update template

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
   title: '浮生纪梦',
   author: '纪华裕',
-  email: 'i@jihuayu.com',
+  email: 'jihuayu123@gmail.com',
   link: 'https://blog.jihuayu.com',
   description: 'This gonna be an awesome website.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/jihuayu123',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
