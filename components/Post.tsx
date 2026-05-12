54271c92b2bf78807071c67588b9b139a93a6bd5
jihuayu <jihuayu123@gmail.com>
2026-05-12 10:08:18 +0800

    动画更新

--- components/Post.tsx
+++ components/Post.tsx
@@ -27,39 +27,46 @@ interface PostProps {
 
 export default function Post(props: PostProps) {
   const { post, document, fullWidth = false, linkPreviewMap = {}, pageLinkMap = {}, pagePreviewMap = {} } = props
+  const isPost = post.type[0] !== 'Page'
 
   return (
     <article className={cn('flex flex-col', fullWidth ? 'md:px-24' : 'items-center')}>
-      <h1 className={cn(
-        'w-full font-bold text-3xl text-black dark:text-zinc-100',
-        !fullWidth && `${ARTICLE_CONTENT_MAX_WIDTH_CLASS} px-4`
-      )}>
-        {post.title}
-      </h1>
-      {post.type[0] !== 'Page' && (
-        <nav className={cn(
-          'w-full flex mt-7 items-start text-gray-500 dark:text-gray-400',
+      <header
+        className={cn(
+          'blog-post-hero w-full',
           !fullWidth && `${ARTICLE_CONTENT_MAX_WIDTH_CLASS} px-4`
-        )}>
-          <div className="flex mb-4">
-            <a href={config.socialLink || '#'} className="flex">
-              <p className="ml-2 md:block">{config.author}</p>
-            </a>
-            <span className="block">&nbsp;/&nbsp;</span>
-          </div>
-          <div className="mr-2 mb-4 md:ml-0">
-            {formatDate(post.date, config.lang, config.timezone)}
-          </div>
-          {post.tags && (
-            <div className="flex flex-nowrap max-w-full overflow-x-auto article-tags">
-              {post.tags.map(tag => (
-                <TagItem key={tag} tag={tag} />
-              ))}
+        )}
+      >
+        <h1 className="font-bold text-3xl text-black dark:text-zinc-100">
+          {post.title}
+        </h1>
+        {isPost && (
+          <nav className="flex mt-7 items-start text-gray-500 dark:text-gray-400">
+            <div className="flex mb-4">
+              <a href={config.socialLink || '#'} className="flex">
+                <p className="ml-2 md:block">{config.author}</p>
+              </a>
+              <span className="block">&nbsp;/&nbsp;</span>
             </div>
-          )}
-        </nav>
-      )}
-      <div className="self-stretch -mt-4 relative">
+            <time className="mr-2 mb-4 md:ml-0">
+              {formatDate(post.date, config.lang, config.timezone)}
+            </time>
+            {post.tags && (
+              <div className="flex flex-nowrap max-w-full overflow-x-auto article-tags">
+                {post.tags.map(tag => (
+                  <TagItem key={tag} tag={tag} />
+                ))}
+              </div>
+            )}
+          </nav>
+        )}
+        {isPost && post.summary && (
+          <p className="blog-post-summary mb-8 leading-8 text-gray-700 dark:text-gray-300">
+            {post.summary}
+          </p>
+        )}
+      </header>
+      <div className="blog-post-body self-stretch -mt-4 relative">
         <div className={fullWidth ? 'w-full px-4 md:px-24' : `mx-auto w-full ${ARTICLE_CONTENT_MAX_WIDTH_CLASS} px-4`}>
           <NotionRenderer document={document} linkPreviewMap={linkPreviewMap} pageLinkMap={pageLinkMap} pagePreviewMap={pagePreviewMap} />
         </div>
